8d856752716e528919c13c2a66cfb7d9420d004e
dinoli <dinolii1220@gmail.com>
2021-01-04 05:49:58 -0500

    Add multi linguistic support

--- fetch_note.py
+++ fetch_note.py
@@ -7,6 +7,7 @@ import json
 import re
 import requests
 requests.session().keep_alive = False  # 及时释放
+LANGUAGE_TO_FILE_NAME = {'ENGLISH': 'en.md', 'CHINESE': 'zh.md'}
 
 
 def get_info(comments_url, token):
@@ -31,20 +32,55 @@ def generate_header(header_text):
     author = partition('author', 'tags')
     tags = partition('tags')
     tags = ''.join(list(filter(lambda x:x not in ['\t', '\r', '\n', ' '], tags))).replace('-', '\n    - ')
-    mk = f'---\nlayout: post\ntitle: "{title}"\nsubtitle: "{subtitle}"\ndate: {date}\nauthor: "{author}"\ntags:{tags}\n---\n'
-    return mk
+    mk = f'---\nlayout: post\ntitle: "{title}"\nsubtitle: "{subtitle}"\ndate: {date}\nmultilingual: True\nauthor: "{author}"\ntags:{tags}\n---\n'
+    return mk, {'title': title,
+                'subtitle': subtitle,
+                'date': date,
+                'author': author,
+                'tags': list(map(lambda y: y.strip().replace('-', '').strip(),
+                                 list(filter(lambda x: '-' in x,
+                                             tags.split('\n')))))}
 
 
 def to_markdown(page, coms):
     content_body_index = page['body'].index('##')
     content_header = page['body'][:content_body_index]
-    mk = generate_header(content_header)
+    header, info = generate_header(content_header)
+    mk = header
+    label = [label.get('name', None) for label in page['labels']]
+    label = label[0] if label != [] and label[0].upper() in LANGUAGE_TO_FILE_NAME else None
+    campus, course_code = info['tags'][0].upper(), info['title']
     content_body = page['body'][content_body_index:]
-    mk += content_body+'\n'
+    # Directory for storing multi-linguistic template
+    directory = os.path.join('./_includes/posts', f'{campus}/{course_code.upper()}')
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+    # Save content body to the correct language
+    save_file(os.path.join(directory, LANGUAGE_TO_FILE_NAME[label.upper()]),
+              content_body)
+    for language in LANGUAGE_TO_FILE_NAME:
+        if language.upper() != label.upper():
+            file_directory = os.path.join(directory,
+                                          LANGUAGE_TO_FILE_NAME[language.upper()])
+            # Put under construction for other languages
+            if not os.path.exists(file_directory):
+                with open(os.path.join('./template/construction',
+                                       LANGUAGE_TO_FILE_NAME[language.upper()])) as construction_file:
+                    construction = ''.join(construction_file.readlines())
+                    save_file(file_directory, construction)
+    with open('./template/multi-linguistic.md') as f:
+        content = ''.join(f.readlines())
+        content = content.replace('directory', f'{campus}/{course_code.upper()}')
+    mk += content
 
     for com in coms:
         mk += com['body']+"\n"
     return mk
+
+
+def save_file(directory, content):
+    with codecs.open(directory, 'w', 'utf-8') as f:
+        f.write(content)
 
 
 if __name__ == '__main__':
